b95f255ae4058b86e5a438225ea6d7e7c0fd524d
luckyjek <thxjek@gmail.com>
2021-08-17 09:00:59 +0900

    Fix Tiketing.sol & Tiket.js

--- miniDApp/solidity/Tiketing.sol
+++ miniDApp/solidity/Tiketing.sol
@@ -1,34 +1,34 @@
-pragma solidity 0.8.6;
+// pragma solidity 0.8.6;
 
-contract Tiketing{
+// contract Tiketing{
  
-    mapping(address => uint) public balance;
-    // string message;
-    // uint num;
+//     mapping(address => uint) public balance;
+//     // string message;
+//     // uint num;
 
     
-    //누가 , 무엇을 , 얼마 
-    //user가 나의컨트랙트로 보내는 입금함수 
-    // function deposit(uint amount, string memory message, uint num ) public{
-    //     balance[msg.sender] += amount; 
+//     //누가 , 무엇을 , 얼마 
+//     //user가 나의컨트랙트로 보내는 입금함수 
+//     // function deposit(uint amount, string memory message, uint num ) public{
+//     //     balance[msg.sender] += amount; 
         
-    // }
-    function deposit( ) public payable{
-        // require(msg.sender >= 0.5 ether);
-        // balance[msg.sender] += msg.value; 
+//     // }
+//     function deposit( ) public payable{
+//         // require(msg.sender >= 0.5 ether);
+//         // balance[msg.sender] += msg.value; 
         
-        balance[msg.sender] += msg.value;
+//         balance[msg.sender] += msg.value;
         
-    }
+//     }
     
-    function getBalance() public view returns (uint) {
-        return balance[msg.sender];
-    }
+//     function getBalance() public view returns (uint) {
+//         return balance[msg.sender];
+//     }
     
-    receive() external payable{}
+//     receive() external payable{}
     
     
-}
+// }
 
 pragma solidity ^0.4.23;
 
---
+++ miniDApp/tiket.js
@@ -0,0 +1,26 @@
+let tiketList = [
+    {
+        id: 0,
+        type: "YOSIGO",
+        picture: "images/file1_1622428115l0o94ushqo.jpg",
+        price: 1,
+    },
+    {
+        id: 1,
+        type: "BLUE ROOM",
+        picture: "images/file1_1622428115l0o94ushqo.jpg",
+        price: 1,
+    },
+    {
+        id: 2,
+        type: "YOSIGO_1",
+        picture: "images/file1_1622428115l0o94ushqo.jpg",
+        price: 1,
+    },
+    {
+        id: 3,
+        type: "BLUE ROOM_2",
+        picture: "images/file1_1622428115l0o94ushqo.jpg",
+        price: 1,
+    },
+];
